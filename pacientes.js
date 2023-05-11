2b2caf6676c746d0d39d308d897aacae5dbeddc4
carol-herms <carolherms27@gmail.com>
2023-05-11 17:38:22 -0300

    First Commit

---
+++ pacientes.js
@@ -0,0 +1,20 @@
+const primeiroNome = prompt ("Informe o nome do paciente")
+const sobrenome = prompt ("Informe o sobrenome do paciente")
+const campoDeEstudo = prompt ("Qual a especialidade?")
+const anoDeNascimento = prompt ("Qual o ano de nascimento?")
+
+alert(
+    "Paciente cadastrado!" 
+)
+
+alert ( "Nome Completo:" +" "+ primeiroNome + " " + sobrenome
+)
+
+alert("Especialidade:" + " " + campoDeEstudo 
+    
+    
+)
+
+alert(
+    "Idade:" + (2023 - anoDeNascimento)
+)
